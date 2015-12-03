523e45141d6c267b6400d8bc24af0cfc5eacfdae
Brendan Wilde <brendanwilde@gmail.com>
2015-12-03 20:54:56 +1100

    deleted dupes

--- rottenpotatoes/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rottenpotatoes::Application.routes.draw do
-	resources :movies
-	get '/movies/find-by-director/:id/' => 'movies#find_by_director', as: 'find_by_director'
-	
-	root 'movies#index'
-end
